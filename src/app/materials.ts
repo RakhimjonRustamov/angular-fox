7f61bac07294dc33f9c76e92d7f08d26a3d86596
RakhimjonRustamov <rakhimjon.rustamov@gmail.com>
2022-01-24 18:45:05 +0500

    New console logs git push -f

--- src/app/materials.ts
+++ src/app/materials.ts
@@ -1,3 +1,6 @@
 function all() {
+    console.log("Resolve Feature Branch");
+    console.log("Before merge changes to master");
+    console.log("git push -f");
     return "Changes from Feature Branch";
 }
